8807d8623a91211a42f443626924df6949e80f57
Achowba <princeatauba@gmail.com>
2017-08-18 09:56:18 +0100

    task completed

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$db = "hng";
+
+// Create connection to database
+$connect = mysqli_connect($servername, $username, $password, $db);
+
+// Check connection status
+if (!$connect) {
+    die ("Connection Failed: " . mysqli_connect_error());
+}
+
+// Retrieve from database
+$sql = 'SELECT id, firstname, dateadded FROM test';
+$query = mysqli_query($connect, $sql);
+
+?>
+
+<!doctype html>
+<html>
+<head>
+    <title>Hotels NG | First Task</title>
+</head>
+<body>
+<?php
+
+echo '<table>
+        <thead>
+            <tr>
+                <th>S/N</th>
+                <th>NAME</th>               
+                <th>DATE ADDED</th>
+            </tr>
+        </thead>
+        <tbody>
+      ';
+
+while ($row = mysqli_fetch_array($query)) {
+    echo '
+    <tr>
+        <td>' . $row['id'] . '</td>
+        <td>' . $row['firstname'] . '</td>
+        <td>' . $row['dateadded'], '</td>            
+    </tr>';
+}
+echo '
+    </tbody>
+</table>';
+?>
+</body>
+</html>
